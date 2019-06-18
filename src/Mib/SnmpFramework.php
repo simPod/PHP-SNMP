be4017c630279ffaaafbdf87ea415414b6994cd3
Jakub Chábek <jakub.chabek@cdn77.com>
2019-06-18 14:31:42 +0200

    tmp rename mib

--- src/Mib/SnmpFramework.php
+++ src/Mib/SnmpFramework.php
@@ -6,7 +6,7 @@ namespace SimPod\PhpSnmp\Mib;
 
 use SimPod\PhpSnmp\Transport\Snmp;
 
-final class SNMPFramework
+final class SnmpFramework
 {
     public const OID_SNMP_ENGINE_TIME = '.1.3.6.1.6.3.10.2.1.3';
 
